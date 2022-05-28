fde7770db2f0a3f91a530c113f396f55dc7afc94
fabiCoseglia <fabiancoseglia@gmail.com>
2022-05-28 14:03:52 -0300

    add array dias & log en dia.js

--- dia.js
+++ dia.js
@@ -1,1 +1,5 @@
 console.log("sabado");
+let dias = ['domingo','lunes', 'martes','miercoles','jueves','viernes','sabado'];
+console.log('.........................................................')
+let diasListados = dias.forEach((dia,i) =>{ console.log(`los dias de la semana son: ${i+1}.- ${dia}`)})
+console.log('.........................................................')
